9b06a8cba24bb2506ea2468079fa78d853505d89
NicoElbers <Ryojipog@gmail.com>
2024-01-01 19:29:28 +0100

    fixed ' issues

--- lua/custom/plugins/brackets.lua
+++ lua/custom/plugins/brackets.lua
@@ -2,6 +2,30 @@ return {
   {
     event = "InsertEnter",
     'm4xshen/autoclose.nvim',
-    opts = {},
+    opts = {
+      keys = {
+        ["("] = { escape = false, close = true, pair = "()" },
+        ["["] = { escape = false, close = true, pair = "[]" },
+        ["{"] = { escape = false, close = true, pair = "{}" },
+        ["<"] = { escape = false, close = true, pair = "<>" },
+
+        [">"] = { escape = true, close = false, pair = "<>" },
+        [")"] = { escape = true, close = false, pair = "()" },
+        ["]"] = { escape = true, close = false, pair = "[]" },
+        ["}"] = { escape = true, close = false, pair = "{}" },
+
+        ['"'] = { escape = true, close = true, pair = '""' },
+        ["'"] = { escape = true, close = false, pair = "''" },
+        ["`"] = { escape = true, close = true, pair = "``" },
+      },
+      options = {
+        disabled_filetypes = { "text" },
+        disable_when_touch = false,
+        touch_regex = "[%w(%[{]",
+        pair_spaces = false,
+        auto_indent = true,
+        disable_command_mode = false,
+      },
+    },
   },
 }
