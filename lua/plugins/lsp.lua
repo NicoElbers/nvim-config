0e5d828b56ac5f61504ec1c03ebe7f34c9c08898
Nico Elbers <nico.b.elbers@gmail.com>
2024-02-23 12:08:33 +0100

    chore: remove unneeded diagnostic config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,22 +62,6 @@ return {
                     })
                 end,
             })
-
-            vim.diagnostic.config({
-                virtual_text = true,
-                signs = true,
-                update_in_insert = true,
-                underline = true,
-                severity_sort = false,
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                },
-            })
         end,
     },
     {
