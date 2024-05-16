5b070da3943fca78ebc7da5b48a4ebd8cc55d9c4
Nico Elbers <nico.b.elbers@gmail.com>
2024-05-16 23:53:53 +0200

    ensure install some parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup({
             auto_install = true,
-            ensure_installed = {},
+            ensure_installed = { "lua", "rust", "c", "zig" },
             ignore_install = {},
             sync_install = true,
             modules = {},
