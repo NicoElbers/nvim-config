0325c28c0f3c3c52906c486134bccdcb89ca9018
Nico Elbers <nico.b.elbers@gmail.com>
2024-05-16 23:56:09 +0200

    disable deprecated messages

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+---@diagnostic disable-next-line: duplicate-set-field
+vim.deprecate = function() end
+
 -- Make sure lazy exists
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
