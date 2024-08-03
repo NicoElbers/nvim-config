d2c6825f410499385d024cf3324ebda464b9dd5a
Nico Elbers <nico.b.elbers@gmail.com>
2024-08-03 19:43:06 +0200

    update to latest flake template fixes

--- flake.nix
+++ flake.nix
@@ -56,6 +56,9 @@
     };
 
     configuration = { pkgs, ... }: 
+    let
+      patchUtils = nv.patchUtils;
+    in 
     {
       # The path to your neovim configuration.
       luaPath = ./.;
@@ -223,10 +226,7 @@
       extraPython3WrapperArgs = [ ];
 
       # Extra lua packages for the neovim lua runtime.
-      luaPackages = [ ]; # FIXME: why is this not being used????
-
-      # Extra packages that should be available at build time for packages.
-      propagatedBuildInputs = [ ]; # FIXME: why is this not being used????
+      luaPackages = [ ];
 
       # Extra shared libraries available at runtime.
       sharedLibraries = [ ];
@@ -240,19 +240,16 @@
       # Must be provided as a list of strings.
       extraConfig = [ ];
 
-      # Custom subsitutions you want the patcher to make. They must be provided
-      # in the following format
-      # {
-      #   type = "string" or "plugin";
-      #   from = string literal or plugin url
-      #   to = The string literal replacing "from"
-      #   extra = for type "string" this is the "key" of the string as seen here:
-      #           `key = "string"`
-      #           leave as null if you don't care about the key.
-      #           For type "plugin" this is the plugin name
-      # }
-      # TODO: Expose functions to make this much easier on the user
-      customSubs = [ ];
+      # Custom subsitutions you want the patcher to make. Custom subsitutions 
+      # can be generated using
+      customSubs = with pkgs.vimPlugins patchUtils; [];
+            # For example, if you want to add a plugin with the short url
+            # "cool/plugin" which is in nixpkgs as plugin-nvim you would do:
+            # ++ (patchUtils.githubUrlSub "cool/plugin" plugin-nvim);
+            # If you would want to replace the string "replace_me" with "replaced" 
+            # you would have to do:
+            # ++ (patchUtils.stringSub "replace_me" "replaced")
+            # For more examples look here: https://github.com/NicoElbers/nv/blob/main/subPatches.nix
 
       settings = {
         # Enable the NodeJs provider
