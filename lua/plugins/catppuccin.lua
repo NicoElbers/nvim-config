6fa7b17a2bfd1ea07d2c3983d6bf017637a6484e
Nico Elbers <nico.b.elbers@gmail.com>
2024-09-26 12:07:55 +0200

    Transparent background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,9 @@ return {
     priority = 1000,
     config = function()
         -- colorscheme catppuccin " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+        require("catppuccin").setup({
+            transparent_background = true,
+        })
         vim.cmd.colorscheme("catppuccin-mocha")
     end,
 }
