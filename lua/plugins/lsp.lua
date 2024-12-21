415495df6b0705c23a6a87a1e56f54c026583947
Nico Elbers <nico.b.elbers@gmail.com>
2024-12-21 18:25:46 +0100

    lsp: setup lua_ls for config dev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,6 +64,30 @@ return {
                 on_attach = utils.on_attach,
                 cmd = { "lua-language-server" },
                 capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua youre using
+                            version = "LuaJIT",
+                            -- Setup your `runtimepath` for Neovim
+                            path = vim.split(package.path, ";"),
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                            -- Adjust workspace check to reduce warnings about workspace folders
+                            checkThirdParty = false,
+                        },
+                        telemetry = {
+                            -- Disable telemetry to prevent data collection
+                            enable = false,
+                        },
+                    },
+                },
             })
 
             -- Markdown
